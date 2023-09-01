542845adf95f86aa961b652a630f81379b4378ac
koomekevo <koome.kevo@gurucrafts.net>
2023-09-01 11:44:53 +0300

    Add HomeScreen, LoginScreen and SignUpScreen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen, LoginScreen, SignUpScreen } from "./screens"; 
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>App</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
