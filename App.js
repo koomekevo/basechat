324bb149b2ae1143ff594c1da9db4cae4d5a9130
koomekevo <koome.kevo@gurucrafts.net>
2023-09-01 11:14:49 +0300

    Install Project Packages and Setup Project

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center space-y-6">
-      <Text className="text-3xl font-bold text-zinc-600">Counter</Text>
-      <View className="flex items-center justify-center p-12 border border-zinc-400 rounded-xl">
-        <Text className="text-3xl font-bold text-zinc-600">0</Text>
-      </View>
-
-      <View className="flex flex-row items-center justify-evenly w-full">
-        <TouchableOpacity className="py-3 px-5  bg-blue-500  rounded-xl flex items-center justify-center">
-          <Text className="text-3xl font-bold text-gray-50">+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="py-3 px-5  bg-blue-500 rounded-xl flex items-center justify-center">
-          <Text className="text-3xl font-bold text-gray-50">-</Text>
-        </TouchableOpacity>
-      </View>
+    <View className="flex-1 items-center justify-center">
+      <Text>App</Text>
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
