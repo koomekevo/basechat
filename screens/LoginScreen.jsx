420c4e9668c393a2a61e671c32e36404c6463bb5
koomekevo <koome.kevo@gurucrafts.net>
2023-09-01 12:06:14 +0300

    Add BGImage and Logo to LoginScreen

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Dimensions } from "react-native";
+import React from "react";
+
+import { BGImage, Logo } from "../assets";
 
 const LoginScreen = () => {
+  const screenWidth = Math.round(Dimensions.get("window").width);
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <View className="flex-1 items-center justify-start">
+      <Image
+        source={BGImage}
+        resizeMode="cover"
+        className="h-96"
+        style={{ width: screenWidth }}
+      />
 
-export default LoginScreen
+      {/* Main View */}
+      <View className="w-full h-full bg-white rounded-tl-[90px] -mt-44 flex items-center justify-start py-6 space-y-6">
+        <Image source={Logo} className="w-16 h-16" resizeMode="contain" />
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
