9dba45209abb3e763a59a735c77b4a5011211e9b
koomekevo <koome.kevo@gurucrafts.net>
2023-09-18 21:41:28 +0300

    Install Firebase

---
+++ config/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPvzgvHcIBNhodYZu7Bs_dqRhX7Ei8BM4",
+  authDomain: "basechatfirebaseapp.firebaseapp.com",
+  projectId: "basechatfirebaseapp",
+  storageBucket: "basechatfirebaseapp.appspot.com",
+  messagingSenderId: "692754963632",
+  appId: "1:692754963632:web:328bad05828e2a2a046f75",
+  measurementId: "G-3LBP6D7CQX",
+};
