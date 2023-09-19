af7b6b1f11c60e530b2a297697dc2e872a4a5ea5
koomekevo <koome.kevo@gurucrafts.net>
2023-09-19 16:33:46 +0300

    Add firebase.config.js

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,3 +1,7 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAPvzgvHcIBNhodYZu7Bs_dqRhX7Ei8BM4",
   authDomain: "basechatfirebaseapp.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
   appId: "1:692754963632:web:328bad05828e2a2a046f75",
   measurementId: "G-3LBP6D7CQX",
 };
+
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+const firebaseAuth = getAuth(app);
+const firestoreDB = getFirestore(app);
+
+export { app, firebaseAuth, firestoreDB };
